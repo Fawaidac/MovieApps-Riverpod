f23c81f1b6217348cf6ec30c331b7be5dace78e6
fawaidac <fwdachmd@gmail.com>
2024-11-09 13:46:20 +0700

    add env

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,7 +1,8 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConfig {
   static const String apiUrl = 'https://api.themoviedb.org/3/movie';
   static const String apiSearchUrl = 'https://api.themoviedb.org/3';
   static const String imageUrl = 'https://image.tmdb.org/t/p/w500';
-  static const String token =
-      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTc4NDg0Zjk1ZDI3ZDBjMzMyZTg3MDEyOWQyZjY0NSIsIm5iZiI6MTczMDIyOTM4OS4yMzYxNjUzLCJzdWIiOiI2NjYzM2FkODc1NWMxYzEzYWRmMGVkN2MiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.1OSY4s6LofPCfMX2jOfdxehloUqkm26f_1xOIH3ZGaA';
+  static String token = dotenv.env['TMDB_API_KEY'] ?? '';
 }
